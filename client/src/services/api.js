ce50a3ab8e3030f8b14614308006366ec5978e5b
rishabhjain1712 <rishabh201224@gmail.com>
2025-12-07 23:16:53 +0530

    hardcoded uri

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://nua-assignment-mrpz.onrender.com',
+  baseURL: 'https://nua-assignment-mrpz.onrender.com',
    headers: {
     'Content-Type': 'application/json'
   }
